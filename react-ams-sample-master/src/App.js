eb348fd56e93eb498d221e373783c5d073120181
Asem-Assi <98389888+Asem-Assi@users.noreply.github.com>
2024-08-22 17:36:37 +0300

    this is just react app with only two component that one send his stream to the new AMS server and the second component just consume that stream from the same new AMS

---
+++ react-ams-sample-master/src/App.js
@@ -0,0 +1,14 @@
+import PublishingComponent from './PublishingComponent';
+import PlayingComponent from './PlayingComponent';
+
+const App = () => {
+  return (
+    <div>
+            <PublishingComponent />
+
+      <PlayingComponent />
+    </div>
+  );
+};
+
+export default App;
